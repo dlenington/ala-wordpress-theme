a839fdca31a5e16bec252b8b51da279d8a5c6944
Dan Lenington <danlening02@gmail.com>
2020-09-01 09:29:35 -0500

    Create front page template file and removed sidebar widget from bottom of homepage

---
+++ front-page.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The template for displaying all the home page.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Education_Hub
+ */
+
+get_header(); ?>
+
+
+
+<?php if ( true === apply_filters( 'education_hub_filter_home_page_content', true ) ) : ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+					endif;
+				?>
+
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+
+
+<?php endif; // End if show home content. ?>
+
+<?php get_footer(); ?>
